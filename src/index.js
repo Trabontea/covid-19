154bcde1d081e47813cccdd2bf72765ca80c788f
Daniel Trabontea <danielmbotea@gmail.com>
2020-05-13 18:23:47 +0300

    json

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
